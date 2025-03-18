4c0b41e8ed70bb94e95c2c17c664007b32022f77
Ralf <ralfduli24@gmail.com>
2025-03-18 23:10:29 +0000

    Created scaffolding for project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <>
+    <Routes>
+      {/* 
+      Routes go here
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/about" element={<About />} /> */}
+    </Routes>
+    </>
+  )
+}
